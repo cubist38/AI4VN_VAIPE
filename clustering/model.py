5298bbd8524da139c411b3977c563b4c0d574a0c
H.T.Vu <trongvulqd@gmail.com>
2022-07-24 18:17:33 +0700

    modify some parts of models

--- clustering/model.py
+++ clustering/model.py
@@ -1,28 +1,32 @@
-import torch
+from torch import nn
 from torchvision.models.mobilenetv3 import mobilenet_v3_large, mobilenet_v3_small
 from torchvision.models.resnet import resnet18
 
-class _Identity(torch.nn.Module):
+class _Identity(nn.Module):
+    '''
+        This class is used when we don't want to calculate anything in classifier layers
+    '''
     def __init__(self):
         super(_Identity, self).__init__()
         
     def forward(self, x):
         return x
 
-class MobilenetV3(torch.nn.Module):
+class MobilenetV3(nn.Module):
     def __init__(self, option='small', pretrained=False):
+        super(MobilenetV3, self).__init__()
         self.model = mobilenet_v3_large(pretrained=pretrained) if option != 'small' else mobilenet_v3_small(pretrained=pretrained)
-        self.model.classifier = self.model.classifier[:1] # just using one Linear Layer in model classifier
-        # self.model.classifier = _Identity # using when dont wanna using classifier layers
+        self.model.classifier = self.model.classifier[:2] # just using one Linear Layer + Hardswish in model classifier
 
-    def predict(self, input):
+    def forward(self, input):
         return self.model(input)
 
-class Resnet18(torch.nn.Module):
+class Resnet18(nn.Module):
     def __init__(self, pretrained=False):
+        super(Resnet18, self).__init__()
         self.model = resnet18(pretrained=pretrained)
-        # self.model.fc = _Identity() # using when dont wanna using classifier layers
+        self.model.fc = nn.Flatten()
         
-    def predict(self, input):
+    def forward(self, input):
         return self.model(input)
 
