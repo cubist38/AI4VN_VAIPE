8515ea4899eded4ba78c02e964e86f3d5419f50d
H.T.Vu <trongvulqd@gmail.com>
2022-07-24 18:18:28 +0700

    add config for clustering algorithms

---
+++ clustering/config.py
@@ -0,0 +1,6 @@
+IMAGE_SIZE = 224    # resize image
+N_COMP = 150        # PCA components
+
+# For DBSCAN. We need to do some experiments to find the best value for them.
+THRES = 1.67        # epsilon
+MINPTS = 155        # minPts, or min_samples
