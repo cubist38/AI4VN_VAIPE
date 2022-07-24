9123d28c3bca05c315102c5f3c434bb09cd14d1e
mathesics <gaxe1102@gmail.com>
2022-07-24 11:41:38 +0700

    delete inheritance nn.Module and fix model classifier of mobilenetv3

--- clustering/model.py
+++ clustering/model.py
@@ -9,16 +9,16 @@ class _Identity(torch.nn.Module):
     def forward(self, x):
         return x
 
-class MobilenetV3(torch.nn.Module):
+class MobilenetV3():
     def __init__(self, option='small', pretrained=False):
         self.model = mobilenet_v3_large(pretrained=pretrained) if option != 'small' else mobilenet_v3_small(pretrained=pretrained)
-        self.model.classifier = self.model.classifier[:1] # just using one Linear Layer in model classifier
+        self.model.classifier = self.model.classifier[:2] # just using a Linear layer and its activation layer in model classifier
         # self.model.classifier = _Identity # using when dont wanna using classifier layers
 
     def predict(self, input):
         return self.model(input)
 
-class Resnet18(torch.nn.Module):
+class Resnet18():
     def __init__(self, pretrained=False):
         self.model = resnet18(pretrained=pretrained)
         # self.model.fc = _Identity() # using when dont wanna using classifier layers
