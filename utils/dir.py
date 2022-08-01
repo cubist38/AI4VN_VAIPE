13d59569071d4966c9fb0f8422cce1e1efeef1c8
H.T.Vu <trongvulqd@gmail.com>
2022-08-02 03:10:52 +0700

    remove old files

--- utils/dir.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def create_directory(directory):
-    dirs = directory.split('/')
-    path = ''
-    for folder in dirs:
-        path = os.path.join(path, folder)
-        if not os.path.exists(path):
-            os.makedirs(path)
--- utils/io.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import json
-
-def read_image(image_path: str):
-    return cv2.imread(image_path)
-
-def read_bbox(bbox_path: str):
-    with open(bbox_path) as f:
-        return json.load(f)
