5270502e95fac3e68a65b39468f9ec462ee76c8e
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2022-08-31 10:12:09 +0700

    update

--- algorithms/classification/test.py
+++ algorithms/classification/test.py
@@ -3,6 +3,34 @@ import torch
 from tqdm import tqdm
 import numpy as np
 
+def get_eval_prediction(model: nn.Module, loader, device):
+    model.eval()
+    idx = 0
+    predictions = torch.zeros(len(loader.dataset), 1)
+    confidences = torch.zeros(len(loader.dataset), 1)
+    labels = torch.zeros(len(loader.dataset), 1)
+    pred_list = []
+    with torch.no_grad():
+        pbar = tqdm(enumerate(loader), total = len(loader))
+        for it, (img, label) in pbar:
+            last_it = it
+            batch_size = img.size(0)
+            img = img.to(device)
+
+            pred = model(img)
+            pred_list.append(pred)
+            pred_label = torch.argmax(pred, 1).unsqueeze(-1)
+            predictions[idx:idx + batch_size, :] = pred_label
+            labels[idx:idx + batch_size, :] = torch.argmax(label, 1).unsqueeze(-1)
+            confidences[idx:idx + batch_size, :] = torch.amax(pred, 1).unsqueeze(-1)
+
+            idx += batch_size
+            # pbar.set_description(description)
+    predictions = np.squeeze(predictions.cpu().numpy())
+    labels = np.squeeze(labels.cpu().numpy())
+    confidences = np.squeeze(confidences.cpu().numpy())
+    return predictions, labels, confidences
+
 def get_prediction(model: nn.Module, loader, device, get_predict_score: bool = False):
     model.eval()
     idx = 0
