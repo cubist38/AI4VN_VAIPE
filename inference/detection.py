8b2c9c6c95a662eafbc9142b74d5b23132054d15
cubist38 <74866985+cubist38@users.noreply.github.com>
2022-09-06 11:54:36 +0700

    Debug code

--- inference/detection.py
+++ inference/detection.py
@@ -19,15 +19,23 @@ def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, c
     if not os.path.exists(crop_cfg['crop_img_dir']):
         os.mkdir(crop_cfg['crop_img_dir'])
 
+    def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, crop_cfg: Dict, model_name: str = 'yolov5') -> Dict:
+    '''
+        Run Object Detection model to return the result of images in image_folder
+        Returns:
+            A dictonary with:
+            - key: path_to_image
+            - value: a list of objects with the following information: (xmin, ymin, xmax, ymax, label)
+    '''
+    detection_results = {}
+
     if model_name == 'yolov5':
-        model = torch.hub.load('algorithms/detection/yolo/yolov5', 'custom', path=detection_cfg['weight_bbox_only_path'], source='local')
-        model1 = torch.hub.load('algorithms/detection/yolo/yolov5', 'custom', path=detection_cfg['weight_bbox_label_path'], source='local')
+        model = torch.hub.load('detection/yolo/yolov5', 'custom', path=detection_cfg['weight_bbox_only_path'], source='local')
+        model1 = torch.hub.load('detection/yolo/yolov5', 'custom', path=detection_cfg['weight_bbox_label_path'], source='local')
         model.conf = detection_cfg['model_conf']
         model1.conf = detection_cfg['model_conf']
         image_files = os.listdir(image_folder)
         crop_detection_map = {}
-        
-        cnt = 0
         
         for id, file in enumerate(image_files):
             print(f'[{id+1}/{len(image_files)}] Running on {file} ...')
@@ -68,10 +76,6 @@ def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, c
                 normalized_bboxes_list = normalized_bbox_coordinate(boxes_list, shape[1], shape[0])
                 normalized_bboxes, scores, labels = weighted_boxes_fusion([normalized_bboxes_list], [scores_list], [labels_list], weights=None, iou_thr=0.55, skip_box_thr=0.0)
                 bboxes = normalized_to_real(normalized_bboxes, shape[1], shape[0])
-
-          #  for i in range(len(bboxes)):
-          #      xmin, ymin, xmax, ymax = bboxes[i][0], bboxes[i][1], bboxes[i][2], bboxes[i][3]
-          #      boxes.append((bboxes[i][0], bboxes[i][1], bboxes[i][2], bboxes[i][3], labels[i], scores[i]))
             
             outputs = model1(img_path, size = 640)
             df = outputs.pandas().xyxy[0]
@@ -90,10 +94,6 @@ def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, c
             
             for i in range(len(bboxes)):
                 xmin, ymin, xmax, ymax = bboxes[i][0], bboxes[i][1], bboxes[i][2], bboxes[i][3]
-                xmin = xmin + 30
-                ymin = ymin + 30
-                xmax = xmax - 30
-                ymax = ymax - 30
                 fin_boxes_list.append([xmin, ymin, xmax, ymax])
                 fin_labels_list.append(labels[i])
                 fin_scores_list.append(scores[i])
@@ -104,12 +104,11 @@ def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, c
                     bbox2 = [label_bbox[0], label_bbox[1], label_bbox[2], label_bbox[3]]
                     if check_iou(bbox1, bbox2, 0.6) == True:
                         fin_labels_list[i] = label_bbox[5]
-                        fin_scores_list[i] = label_bbox[4]
+                        fin_scores_list[i] = (label_bbox[4] + fin_scores_list[i])/2
                         
             for label_bbox in label_boxes:
                 fin_boxes_list.append([label_bbox[0], label_bbox[1], label_bbox[2], label_bbox[3]])
                 fin_labels_list.append(label_bbox[5])
-                #fin_labels_list.append(107)
                 fin_scores_list.append(label_bbox[4])
             
             fin_normalized_bboxes_list = normalized_bbox_coordinate(fin_boxes_list, shape[1], shape[0])
@@ -120,8 +119,7 @@ def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, c
                 
             for i in range(len(fin_bboxes)):
                 xmin, ymin, xmax, ymax = int(fin_bboxes[i][0]), int(fin_bboxes[i][1]), int(fin_bboxes[i][2]), int(fin_bboxes[i][3])
-                bbox_area = (xmax - xmin) * (ymax - ymin)
-                if bbox_area > 96**2 or int(fin_labels[i]) == 107:
+                if int(fin_labels[i]) == 107:
                     crop_image_name = str(i) + '_' + file    
                     crop_img = img[ymin : ymax, xmin : xmax]
                     crop_img = mycropoffset(img, xmin, ymin, xmax, ymax, 5)
@@ -143,5 +141,5 @@ def run_detection(image_folder: str, augment_folder: str, detection_cfg: Dict, c
     else:
         print('This model_name is not valid')
         return None
-        
+    
     return detection_results
